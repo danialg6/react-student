73cc9e3573636a2502c2d3d8547f233204e432c4
tushig <tushig.btr@gmail.com>
2022-04-30 22:42:57 -0500

    update

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Context as StudentContext } from "../context/StudentContext";
 
+import NewStudent from "./NewStudent";
+
 const Student = () => {
   const { state, add, remove } = useContext(StudentContext);
 
@@ -60,46 +62,7 @@ const Student = () => {
           </table>
         </div>
       ) : (
-        <div>
-          <input
-            type="text"
-            name="name"
-            placeholder="name"
-            onChange={(event) => {
-              setStudent({ ...student, name: event.target.value });
-            }}
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="email"
-            onChange={(event) => {
-              setStudent({ ...student, email: event.target.value });
-            }}
-          />
-          <input
-            type="text"
-            name="id"
-            placeholder="id"
-            onChange={(event) => {
-              setStudent({ ...student, id: event.target.value });
-            }}
-          />
-          <button
-            onClick={() => {
-              if (!state.students.find((e) => e.id === student.id)) {
-                add(student);
-                let students = localStorage.getItem("students")
-                  ? JSON.parse(localStorage.getItem("students"))
-                  : [];
-                students.push(student);
-                localStorage.setItem("students", JSON.stringify(students));
-              }
-            }}
-          >
-            submit
-          </button>
-        </div>
+        <NewStudent />
       )}
     </div>
   );
